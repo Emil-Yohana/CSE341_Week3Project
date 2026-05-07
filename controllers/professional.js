88e8925dc438594f9bfdebecb4f3d1d405826e0d
Emil Roding <emilroding27873@gmail.com>
2026-05-07 18:14:37 +1200

    connected api to mongo successfully

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 
 const getData = (req, res) => {
-    mongodb.getDb().db('CSE341').collection('professional').find().toArray().then((lists) => {
+    mongodb.getDb().db('CSE341').collection('Professional').find().toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists[0]);
     });
